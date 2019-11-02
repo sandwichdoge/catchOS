3ff2fc99e522f3e991448f847daeff63ed8a8f37
Thuan Ho <sandwichdoge@gmail.com>
2019-11-02 16:12:27 +0700

    doc framebuffer

--- framebuffer.c
+++ framebuffer.c
@@ -10,6 +10,7 @@
 
 volatile char *fb = (volatile char*)0x000B8000;
 
+// Write character to a cell in 80x24 framebuffer
 void write_cell(unsigned int i, unsigned char c, unsigned char fg, unsigned char bg) {
     fb[i] = c;
     fb[i + 1] = ((fg & 0b00001111) << 4) | (bg & 0b00001111);
