bd0bf5a9894aef911c70039caf4dca8f80ca00d1
sandwichdoge <38368906+sandwichdoge@users.noreply.github.com>
2019-12-04 22:48:42 +0700

    Update loader.s

--- loader.s
+++ loader.s
@@ -14,8 +14,8 @@ align 4                         ; the code must be 4 byte aligned
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
 
-section .bss:
-align 4				; align at 4 bytes
+section .bss:                   ; our stack is in uninitialized data section
+align 4				                  ; align at 4 bytes
 kernel_stack:                   ; label points to beginning of memory
     resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
 
