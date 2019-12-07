5d7aadcc197b6bcecf9ce281226d1811aa45b6a3
Thuan Ho <sandwichdoge@gmail.com>
2019-12-07 11:02:05 +0700

    fix segmentation gdt

--- segmentation.c
+++ segmentation.c
@@ -1,59 +1,29 @@
 #include "segmentation.h"
 #include "gdt.h" // ASM wrapper for lgdt
 
-// https://wiki.osdev.org/GDT_Tutorial
-void seg_encode_gdt_entry(char* target, struct gdt_entry source) {
-    // Check the limit to make sure that it can be encoded
-    if ((source.limit > 65536) && ((source.limit & 0xFFF) != 0xFFF)) {
-        //kerror("You can't do that!");
-    }
-    if (source.limit > 65536) {
-        // Adjust granularity if required
-        source.limit = source.limit >> 12;
-        target[6] = 0xC0;
-    } else {
-        target[6] = 0x40;
-    }
- 
-    // Encode the limit
-    target[0] = source.limit & 0xFF;
-    target[1] = (source.limit >> 8) & 0xFF;
-    target[6] |= (source.limit >> 16) & 0xF;
- 
-    // Encode the base 
-    target[2] = source.base & 0xFF;
-    target[3] = (source.base >> 8) & 0xFF;
-    target[4] = (source.base >> 16) & 0xFF;
-    target[7] = (source.base >> 24) & 0xFF;
- 
-    // And... Type
-    target[5] = source.type;
+// 3 GDT entries: Null descriptor, kernel code segment, kernel data segment.
+struct gdt_entry GDT[3] = {0};
+
+void seg_encode_gdt_entry(int index, unsigned int base_address, unsigned int limit, unsigned char type, unsigned char flags) {
+        GDT[index].base_low = base_address & 0xFFFF;
+        GDT[index].base_middle = (base_address >> 16) & 0xFF;
+        GDT[index].base_high = (base_address >> 24) & 0xFF;
+
+        GDT[index].limit_low = limit & 0xFFFF;
+        GDT[index].limit_and_flags = (limit >> 16) & 0xF;
+        GDT[index].limit_and_flags |= (flags << 4) & 0xF0;
+
+        GDT[index].type = type;
 }
 
-char GDT[8 * 3] = {0}; // 3 GDT entries: Null descriptor, kernel code segment, kernel data segment.
 
 // Init GDT for 4 GiB memory
 void seg_init_gdt() {
-    // Init null descriptor
-    for (unsigned char i = 0; i < 8; i++) {
-        GDT[i] = 0;
-    }
+    // Init null descriptor (already nulled out)
 
     // Init kernel code segment
-    struct gdt_entry kcode_desc;
-    kcode_desc.base = 0x04000000;
-    kcode_desc.limit = 0x03ffffff;
-    kcode_desc.type = 0x9A; // Type code
-    
-    seg_encode_gdt_entry((char*)(GDT + 8), kcode_desc);
-
-    // Init kernel data segment
-    struct gdt_entry kdata_desc;
-    kdata_desc.base = 0x08000000;
-    kdata_desc.limit = 0x03ffffff;
-    kdata_desc.type = 0x92; // Type data
-
-    seg_encode_gdt_entry((char*)(GDT + 16), kdata_desc);
+    seg_encode_gdt_entry(1, 0x0, 0xfffff, 0x9a, 0xc);
+    seg_encode_gdt_entry(2, 0x0, 0xfffff, 0x92, 0xc);
 
     struct gdt asm_lgdt_arg;
     asm_lgdt_arg.address = (unsigned int)GDT;
