0773dcd729136467c014d5e52375811baab67817
Thuan Ho <sandwichdoge@gmail.com>
2019-12-12 15:54:59 +0700

    test 32bit mode memory, clarify with comments kboot.s

--- kboot.s
+++ kboot.s
@@ -1,24 +1,23 @@
 global load_gdt
 global switch_to_pm
 global kboot
-extern write_cstr
 extern kmain
 
 gdt_start:
-
-gdt_null:        ; Null gdt entry, mandatory
+; with this setup we're telling the cpu that kernel code & data segments can be anywhere in 4GB memory
+gdt_null:        ; null gdt entry, mandatory
     dd 0x0
     dd 0x0
 
-gdt_code:        ; Kernel code section
-    dw 0xffff
-    dw 0x0
-    db 0x0
-    db 10011010b
-    db 11001111b
-    db 0x0
+gdt_code:        ; kernel code section
+    dw 0xffff    ; limit (bits 0-15)
+    dw 0x0       ; base (bits 0-15)
+    db 0x0       ; base (bits 16-23)
+    db 10011010b ; type flag
+    db 11001111b ; limit flags (bits 16-19)
+    db 0x0       ; base (bit 24- 31)
 
-gdt_data:         ; Kernel data section
+gdt_data:         ; kernel data section
     dw 0xffff
     dw 0x0
     db 0x0
@@ -28,11 +27,12 @@ gdt_data:         ; Kernel data section
 
 gdt_end:
 
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
 
-; Offsets in GDT
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 ; size of GDT, must be true size - 1
+    dd gdt_start               ; address of the GDT
+
+; Offsets of GDT entries in GDT
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
@@ -41,15 +41,16 @@ load_gdt:                    ; load global descriptor table into processor
     lgdt [gdt_descriptor]
     ret
 
-switch_to_pm:           ; switch from 16-bit real mode to 32-bit protected mode
-    cli
+
+switch_to_pm:              ; switch from 16-bit real mode to 32-bit protected mode
+    cli                    ; must disable interrupts
     ; Next 3 instructions switch us to 32-bit protected mode
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
     jmp CODE_SEG:init_pm   ; put 0x08 in cs (which is an offset in the GDT for kernel code segment)
 
-[bits 32]
+[bits 32]                  ; tell assembler to encode in 16-bit mode instructions
 init_pm:
     mov ax, DATA_SEG
     mov ds, ax
@@ -58,7 +59,7 @@ init_pm:
     mov fs, ax
     mov gs, ax
 
-    mov ebp, 0x90000
+    mov ebp, 0x90000  ;
     mov esp, ebp
     ; call something here, never return from init_pm
     call kmain
--- kmain.c
+++ kmain.c
@@ -2,12 +2,26 @@
 #include "framebuffer.h"
 #include "kboot.h"
 
-char message[] = "hello";
-char greeting[] = "Hi! Welcome to Thuan's OS!";
+char greeting[] = "Hi! Welcome to Thuan's OS! You're now in Protected Mode";
+char msg_true[] = "True";
+char msg_false[] = "False";
+
+void test_memory_32bit_mode() {
+    volatile unsigned char *p = (volatile unsigned char *)3000000; // 32MB for testing 32-bit mode
+    *p = 55;
+
+    if (*p == 55) {
+        write_str(msg_true, 160, sizeof(msg_true));
+    } else {
+        write_str(msg_false, 160, sizeof(msg_true));
+    }
+}
 
 void kmain() {
     serial_defconfig(SERIAL_COM1_BASE);
 
     clr_screen(FB_BLACK);
     write_str(greeting, 0, sizeof(greeting));
+
+    test_memory_32bit_mode();
 }
