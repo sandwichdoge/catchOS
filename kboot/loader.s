72771e2e6b4af60e98700f76086232e8b1f4da3a
Thuan Ho <sandwichdoge@gmail.com>
2019-12-19 18:19:11 +0700

    wip issue kernel expansion

--- kboot/loader.s
+++ kboot/loader.s
@@ -7,12 +7,14 @@ CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
 
-
-section .text:                  ; start of the text (code) section
+section .booter:
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd FLAGS                    ; the flags,
     dd CHECKSUM                 ; and the checksum
+
+section .text:                  ; start of the text (code) section
+align 4
 
 section .bss:                   ; our stack is in uninitialized data section
 align 4				                  ; align at 4 bytes
