a9cbe937a5da21f9ef9077566660f2a03dc0b923
Thuan Ho <sandwichdoge@gmail.com>
2019-12-26 17:35:26 +0700

    split multiboot header into a seperate file

--- kboot/loader.s
+++ kboot/loader.s
@@ -1,17 +1,7 @@
 global loader                   ; the entry symbol for ELF
 extern kboot                    ; kboot will switch to protected mode and call kmain
 
-MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-FLAGS        equ 0x0            ; multiboot flags
-CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
-                                ; (magic number + checksum + flags should equal 0)
 KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
-
-section .multiboot:
-align 4                         ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER             ; write the magic number to the machine code,
-    dd FLAGS                    ; the flags,
-    dd CHECKSUM                 ; and the checksum
 
 section .text:                  ; start of the text (code) section
 loader:                         ; the loader label (defined as entry point in linker script)
