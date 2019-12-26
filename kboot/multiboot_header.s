06c20a732bec8e28e28c8b35ec9c999e1b5f2113
Thuan Ho <sandwichdoge@gmail.com>
2019-12-26 17:37:45 +0700

    fix section name taking into account colon multiboot:

--- kboot/multiboot_header.s
+++ kboot/multiboot_header.s
@@ -3,7 +3,7 @@ FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
 
-section .multiboot:
+section .multiboot
 align 4                         ; the code must be 4 byte aligned
     dd MAGIC_NUMBER             ; write the magic number to the machine code,
     dd FLAGS                    ; the flags,
