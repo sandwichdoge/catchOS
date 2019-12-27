28d15b9c21d5a8e0e9bd9aa3bbad246010b65cbc
Thuan Ho <sandwichdoge@gmail.com>
2019-12-27 10:23:41 +0700

    handle shift key uppercase keyboard

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,5 +1,8 @@
 #ifndef INCLUDE_KEYBOARD_H
 #define INCLUDE_KEYBOARD_H
+#define KEY_LSHIFT 14
+#define KEY_RSHIFT 15
+
 unsigned char read_scan_code();
-unsigned char scan_code_to_ascii(unsigned char scan_code);
+unsigned char scan_code_to_ascii(unsigned char scan_code, int is_shift_depressed);
 #endif
--- keyboard.c
+++ keyboard.c
@@ -1,5 +1,6 @@
 #include "io.h"
 #include "keyboard.h"
+#include "utils/debug.h"
 
 #define KBD_DATA_PORT 0x60
 
@@ -21,9 +22,34 @@ unsigned char sc_table_noshift[256] = {
     0 /*PgDown*/, 0 /*Insert*/, 0 /*Keypad-Del*/, 0, 0, 0, 0 /*F11*/, 0 /*F12*/
 };
 
+unsigned char sc_table_shift[256] = {
+    0, 27, '!', '@', '#', '$', '%', '^', 
+    '&', '*', '(', ')', '_', '+', 8 /*Backspace*/, '\t',
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 
+    'O', 'P', '{', '}', '\n', 17 /*LCtrl*/, 'A', 'S', 
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', 
+    '"', '~', 14 /*LShift*/, '|', 'Z', 'X', 'C', 'V', 
+    'B', 'N', 'M', ',', '.', '/', 15 /*RShift*/, 0, 
+    0 /*LAlt*/, ' ', 26 /*CapsLock*/, 0, 0, 0, 0, 0, 0, 
+    0, 0, 0, 0 /*F10*/, 0 /*NumLock*/, 0 /*ScrollLock*/, 0 /*Home*/, 0 /*Up*/, 
+    0 /*PgUp*/, 0 /*Keypad--*/, 0 /*Left*/, 0 /*Keypad-5*/, 0 /*Right*/, 0 /*Keypad-+*/, 0 /*End*/, 0 /*Down*/, 
+    0 /*PgDown*/, 0 /*Insert*/, 0 /*Keypad-Del*/, 0, 0, 0, 0 /*F11*/, 0 /*F12*/
+};
 
 // https://www.win.tue.nl/~aeb/linux/kbd/scancodes-1.html
-unsigned char scan_code_to_ascii(unsigned char scan_code) {
-    if (scan_code & (1 << 7)) return 0; // Ignore key release
-    return sc_table_noshift[scan_code];
+unsigned char scan_code_to_ascii(unsigned char scan_code, int is_shift_depressed) {
+    if (scan_code & (1 << 7)) { // Key release
+        // Ignore key release, unless it's SHIFT (sc_table_noshift[42]) then return another SHIFT
+        if (scan_code == 170 || scan_code == 182) {
+            return sc_table_noshift[scan_code ^ (1 << 7)];
+        } else {
+            return 0;
+        }
+    }
+
+    if (is_shift_depressed) {
+        return sc_table_shift[scan_code];
+    } else {
+        return sc_table_noshift[scan_code];
+    }
 }
