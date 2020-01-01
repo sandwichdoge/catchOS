c6884e90f7d15af38119968c5d745f037f549e56
sandwichdoge <sandwichdoge@gmail.com>
2020-01-01 16:40:47 +0700

    pageframe_alloc

---
+++ kernel/mmu/pageframe_alloc.c
@@ -0,0 +1,68 @@
+#include "kinfo.h"
+
+static char *pageframe_bitmap = (void*)0;
+static unsigned int pages_total = 0;
+
+static void pageframe_allocator_init() {
+	struct kinfo *kinfo = get_kernel_info();
+    pages_total = (kinfo->phys_mem_upper * 1024) / 4096;
+	// 1 bit represents 1 page in phys mem (4 KiB). We assign just enough for the bitmap of physical memory.
+	pageframe_bitmap = kmalloc_align(pages_total / 8, 4096);
+}
+
+static void pageframe_allocator_set_page(unsigned int page_no) {
+	unsigned int byte_no = page_no / 8;
+	unsigned int carry_bit = byte_no % 8;
+	pageframe_bitmap[byte_no] = (1 << carry_bit);
+}
+
+static void pageframe_allocator_clear_page(unsigned int page_no) {
+	unsigned int byte_no = page_no / 8;
+	unsigned int carry_bit = byte_no % 8;
+	pageframe_bitmap[byte_no] &= ~(1 << carry_bit);
+}
+
+static int pageframe_allocator_get_page(unsigned int page_no) {
+	unsigned int byte_no = page_no / 8;
+    unsigned int carry_bit = byte_no % 8;
+    return ((pageframe_bitmap[byte_no] & (1 << carry_bit)) >> carry_bit);
+}
+
+unsigned int pageframe_addr_from_page(unsigned int page_no) {
+    return (page_no * 4096);
+}
+
+void* pageframe_alloc(unsigned int pages) {
+    if (pages > 8) { // To reduce complexity, please request oly 8 pages or fewer at a time
+        return (void*)0;
+    }
+    // Check every bit to find satisfactory free pages
+    void *ret = (void*)0;
+
+    // Check 1 byte at a time for performance
+    for (unsigned int i = 0; i < pages_total / 8; i++) {
+        if (pageframe_bitmap[i] == 0xff) { // No available pages
+            // Carry on to next byte
+        } else {
+            // Now check the byte whether it has enough available consecutive bits
+            unsigned int bits_looped;
+            for (bits_looped = 0; bits_looped < 8; bits_looped++) {
+                if (pageframe_bitmap[i] & (1 << bits_looped) == 0) break;
+            }
+            bits_looped += 1;
+
+            unsigned int available_bits = (8 - bits_looped);
+            if (available_bits >= pages) { // We got enough available bits in byte
+                unsigned int page_no = i * 8 + (bits_looped - 1);
+                ret = (void*)pageframe_addr_from_page(page_no);
+                break;
+            }
+        }
+    }
+
+    return ret;
+}
+
+void pageframe_free(void *phys_addr, unsigned int pages) {
+
+}
--- kernel/mmu/paging.c
+++ kernel/mmu/paging.c
@@ -10,31 +10,6 @@ extern void enablePaging();
 // Page index 123 in table index 456 will be mapped to (456 * 1024) + 123 = 467067. 467067 * 4 = 1868268 KiB.
 
 unsigned int kernel_page_directory[1024] __attribute__((aligned(4096))); // 1024 page tables in page directory
-char *pageframe_bitmap = (void*)0;
-
-static char* pageframe_allocator_init() {
-	struct kinfo *kinfo = get_kernel_info();
-	// 1 bit represents 1 page in phys mem (4 KiB). Here we assign just enough for the bitmap.
-	pageframe_bitmap = kmalloc_align(((kinfo->phys_mem_upper * 1024) / 4096) / 8, 4096);
-}
-
-static void pageframe_allocator_set_page(unsigned int page_no) {
-	unsigned int byte_no = page_no / 8;
-	unsigned int carry_bit = byte_no % 8;
-	pageframe_bitmap[byte_no] = (1 << carry_bit);
-}
-
-static void pageframe_allocator_clear_page(unsigned int page_no) {
-	unsigned int byte_no = page_no / 8;
-	unsigned int carry_bit = byte_no % 8;
-	pageframe_bitmap[byte_no] &= ~(1 << carry_bit);
-}
-
-static int pageframe_allocator_get_page(unsigned int page_no) {
-	unsigned int byte_no = page_no / 8;
-    unsigned int carry_bit = byte_no % 8;
-    return ((pageframe_bitmap[byte_no] & (1 << carry_bit)) >> carry_bit);
-}
 
 static unsigned int virtual_addr_to_pde(unsigned int virtual_addr) {
 	return virtual_addr >> 22;
@@ -57,8 +32,8 @@ void paging_init() {
 	struct kinfo *kinfo = get_kernel_info();
 
 	//unsigned int phys_mem_size_kb = (kinfo->phys_mem_upper - kinfo->phys_mem_lower); // e.g 31680
-	unsigned int pages_total = kinfo->phys_mem_upper / 4;
-	unsigned int page_tables_total = (pages_total + (1024 - 1)) / 1024; // Round up int division
+	//unsigned int pages_total = kinfo->phys_mem_upper / 4;
+	//unsigned int page_tables_total = (pages_total + (1024 - 1)) / 1024; // Round up int division
 
 	// Allocate memory for 1 page table
 	unsigned int *page_tables = kmalloc_align(4096, 4096);
