ea9047436e72ff04a6a7c6d5aa1b2907d7ab72e9
sandwichdoge <sandwichdoge@gmail.com>
2020-03-15 22:35:09 +0700

    fix unit test pageframe_alloc

--- tests/test_pageframe_alloc.cpp
+++ tests/test_pageframe_alloc.cpp
@@ -5,11 +5,13 @@ extern "C" {
 #include "gtest/gtest.h"
 
 extern unsigned char *_pageframe_bitmap;
+extern unsigned int _pages_total_phys;
 extern int _is_initialized;
 unsigned char pf[1024] = {0};
 
 static void init_pageframe() {
     _pageframe_bitmap = pf;
+    _pages_total_phys = 4096;
     _pageframe_bitmap[0] = 0xff;
     _is_initialized = 1;
 }
@@ -17,5 +19,6 @@ static void init_pageframe() {
 TEST(pageframe_alloc, basic) {
     init_pageframe();
     void *p = pageframe_alloc(7);
+    printf("%p\n", p);
     EXPECT_NE(p, (void*)0);
 }
