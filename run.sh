8889220827d22d17083efd4fb7be1fe932ab6fa3
sandwichdoge <sandwichdoge@gmail.com>
2020-07-01 21:16:57 +0700

    run.sh exit immediately on build failure

--- run.sh
+++ run.sh
@@ -2,7 +2,12 @@
 make debug
 if [ "$?" -ne 0 ]; then
     echo 'Error occurred.'
+    exit "$?"
 fi
 make iso
+if [ "$?" -ne 0 ]; then
+    echo 'Error occurred.'
+    exit "$?"
+fi
 cd output
 bochs -f bochsrc.txt -q
