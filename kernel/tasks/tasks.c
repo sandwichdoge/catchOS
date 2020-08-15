26d75895929d7f85c7f45f877cdbae61fe097a1f
Thuan Ho <sandwichdoge@gmail.com>
2020-08-15 16:25:43 +0700

    lock for sched context switch task_startup() to initialize task on creation

--- kernel/tasks/tasks.c
+++ kernel/tasks/tasks.c
@@ -8,6 +8,7 @@
 #include "utils/debug.h"
 #include "utils/string.h"
 #include "utils/rwlock.h"
+#include "utils/spinlock.h"
 
 #define EFLAGS_IF (1 << 9)
 
@@ -18,6 +19,7 @@ static struct task_struct kmaint = {.interruptible = 0, .counter = 1};  // Initi
 struct task_struct* _current = &kmaint;                                 // Current task that controls local CPU. TASK_STATE should always be TASK_RUNNING here.
 
 struct rwlock lock_tasks;   // R/W lock for _tasks list.
+struct spinlock lock_sched;
 
 // Read current EFLAGS register.
 private
@@ -46,11 +48,19 @@ void on_current_task_return_cb() {
     task_yield();
 }
 
+private
+void task_startup(void (*fp)(void*), void* arg) {
+    _dbg_log("task_startup()\n");
+    spinlock_unlock(&lock_sched);
+    fp(arg);
+}
+
 public
 struct task_struct* task_new(void (*fp)(void*), void* arg, size_t stack_size, int32_t priority) {
     /*
     - Task's stack map -
     [arg]
+    [fp]
     [task's ret addr] - When task function reaches return. Should be addr of on_current_task_return_cb().
     [next eip]
     [reg]
@@ -83,12 +93,14 @@ struct task_struct* task_new(void (*fp)(void*), void* arg, size_t stack_size, in
     _dbg_log("Allocated TCB[%d]:[0x%x], stack top:[0x%x], stack size:[0x%x]\n", pid, newtask, newtask->cpu_state.esp, stack_size);
     *(size_t*)(newtask->cpu_state.esp) = (size_t)arg;
     newtask->cpu_state.esp -= sizeof(size_t);
+    *(size_t*)(newtask->cpu_state.esp) = (size_t)fp;
+    newtask->cpu_state.esp -= sizeof(size_t);
     *(size_t*)(newtask->cpu_state.esp) = (size_t)&on_current_task_return_cb;
     newtask->cpu_state.esp -= (sizeof(struct cpu_state) + sizeof(size_t));
     *(size_t*)(newtask->cpu_state.esp) = get_flags_reg() | EFLAGS_IF;  // Always enable interrupt flag for new tasks.
     newtask->priority = priority;
     newtask->pid = pid;
-    newtask->stack_state.eip = (size_t)fp;
+    newtask->stack_state.eip = (size_t)task_startup;
     newtask->interruptible = 1;
     _nr_tasks++;
 
@@ -122,9 +134,11 @@ void task_switch_to(struct task_struct* next) {
     struct task_struct* prev = _current;
     _current = next;
     //_dbg_log("Switch to pid [%u]\n", next->pid);
-    prev->state = TASK_READY;
+    spinlock_lock(&lock_sched);
     next->state = TASK_RUNNING;
-    cpu_switch_to(prev, next);
+    prev->state = TASK_READY;
+    cpu_switch_to(prev, next);  // If task was just initialized, we'll jump to func pointer, not here. Need to unlock in func?
+    spinlock_unlock(&lock_sched);
 }
 
 private
@@ -193,4 +207,5 @@ inline uint32_t task_getpid() { return _current->pid; }
 public
 void tasks_init() {
     rwlock_init(&lock_tasks);
+    spinlock_init(&lock_sched);
 }
